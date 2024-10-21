fbe13329e05fcfd865570c279cec19c4231772fb
Tom Harke <tom.harke@gmail.com>
2024-10-20 21:07:23 -0700

    progress on hw 1
    changes
     - fixed bugs
        - wired up the missing '+1' in 2's complement subtraction
        - had reversed arithmetic inputs
     - minimal tb with 1 test for each op
     - parameterized width in hopes of exhaustively testing for a smaller
       width than 32
    
    Need to figure out enums

--- hw1/alu.sv
+++ hw1/alu.sv
@@ -5,74 +5,77 @@
  * @date:	2024/10/16
  *
  * @brief
- * implements a 32-bit ALU with flags 
+ * implements a 32-bit ALU with flags
  *
  * @note:  Based on an exercise from Digital Design and Computer Architecture: RISC-V edition by Sarah Harris and David Harris
- */ 
+ */
 
 /* Questions
     - 'self checking'
     - how fold a 32-bit-array into 1 bit of output
 
  */
-module alu();
-// timeunit 1ns/1ns;
-
-// TODO: for testing, make 32 a parameter
-//       then, one test involves setting param to 2 or 3 and doing exhaustive run 
-  logic [31:0] a, b, result; 
-  logic [1:0]  alucontrol; 
-  logic [3:0]  flags; 
-
-  logic Cout;
-  logic arithmetic;
-  logic [31:0] a_pm;
-  logic [31:0] Result;
-  logic [31:0] ResultNeg;
-
-  enum BINOP
+  typedef enum bit [1:0]
     { ADD = 2'b00
     , SUB = 2'b01
     , AND = 2'b10
     , OR  = 2'b11
-    };
+    } BINOP;
 
-  enum
+module alu
+  #(parameter WIDTH = 32)
+  (a,b,alucontrol,result,flags);
+
+// timeunit 1ns/1ns;
+
+// TODO: for testing, make 32 a parameter
+//       then, one test involves setting param to 2 or 3 and doing exhaustive run
+  input  logic [WIDTH-1:0] a, b;
+  // input  BINOP  alucontrol;
+  input  logic [1:0]  alucontrol;
+  output logic [WIDTH-1:0] result;
+  output logic [3:0]  flags;
+
+  logic Cout;
+  logic arithmetic;
+  logic [WIDTH-1:0] b_pm;
+  logic [WIDTH-1:0] sum;
+  logic [WIDTH-1:0] resultNeg;
+
+  typedef enum
     { NEGATIVE
     , ZERO
     , CARRY
     , OVERFLOW
-    };
+    } FLAG;
 
   always_comb
     case (alucontrol[0])
-      0: a_pm =  a;
-      1: a_pm = ~a;
-    end case
+      0: b_pm =  b;
+      1: b_pm = ~b;
+    endcase
 
-  assign {Cout,Sum} = a_pm + b;
+  assign {Cout,sum} = a + b_pm + alucontrol[0];
 
   always_comb
     unique case (alucontrol)
-      ADD, SUB: Result = Sum; // TODO: add 1 ?? AHA: via Cin
-      // ADD: Result = Sum;
-      // SUB: Result = Sum; // TODO: add 1 ??
-      AND: Result = a & b;
-      OR:  Result = a | b;
-    end case
+      ADD, SUB: result = sum;
+      AND:      result = a & b;
+      OR:       result = a | b;
+    endcase
 
 
-  assign ResultNeg       = ~Result;
+  assign resultNeg       = ~result;
   assign arithmetic      = ~alucontrol[1];
   assign flags[OVERFLOW] = arithmetic
-                         & (a[31] ^ Sum[31])
-                         & (~(alucontrol[0] ^ a[31] ^ b[31]))
+                         & (a[WIDTH-1] ^ sum[WIDTH-1])
+                         & (~(alucontrol[0] ^ a[WIDTH-1] ^ b[WIDTH-1]))
                          ;
   assign flags[CARRY]    = arithmetic
                          & Cout
                          ;
-  assign flags[ZERO]     = ... and of all ... ResultNeg[31:0];
-  assign flags[NEGATIVE] = Result[31];
+  assign flags[ZERO]     = &resultNeg[WIDTH-1:0];
+  assign flags[NEGATIVE] = result[WIDTH-1];
 
-  
+
 endmodule: alu
--- hw1/alu_tb.sv
+++ hw1/alu_tb.sv
@@ -5,38 +5,85 @@
  * @date:	2024/10/16
  *
  * @brief
- * implements a testbench for the 32-bit ALU with flags 
+ * implements a testbench for the 32-bit ALU with flags
  *
  * @note:
  *    - Exercise based on one from Digital Design and Computer Architecture: RISC-V edition by Sarah Harris and David Harris
  *    - Draft testbench from Roy Kravitz (roy.kravitz@pdx.edu)
- */ 
+ */
+
+  typedef enum bit [1:0]
+    { ADD = 2'b00
+    , SUB = 2'b01
+    , AND = 2'b10
+    , OR  = 2'b11
+    } BINOP;
+
 module alu_tb();
 timeunit 1ns/1ns;
- 
+
+
+  // parameter WIDTH = 32;
+  parameter WIDTH = 4;
+
   // ALU interface signals
-  logic [31:0] a, b, result; 
-  logic [1:0]  alucontrol; 
-  logic [3:0]  flags; 
+  logic [WIDTH-1:0] a, b, result;
+  logic [1:0]  alucontrol;
+  // BINOP  alucontrol;
+  logic [3:0]  flags;
   // TODO:  Declare your other testbench variables here
-  
-  import "DPI-C" function string getenv(input string env_name);  
- 
-  alu DUT (
+
+  import "DPI-C" function string getenv(input string env_name);
+
+  alu #(WIDTH) DUT (
 	.a(a),
 	.b(b),
 	.alucontrol(alucontrol),
 	.result(result),
 	.flags(flags)
-  ); 
- 
- 
+  );
+
+
   initial begin: display_working_dir
+    //alucontrol = 0;
     $display("ECE 351 Fall 2024: ALU testbench - Tom Harke (harke@pdx.edu)");
-    $display("Sources: %s\n", getenv("PWD")); 
+    $display("Sources: %s\n", getenv("PWD"));
   end: display_working_dir
- 
+
+  initial begin: foo
+    alucontrol = ADD;
+  end: foo
+
+  initial begin: drive_tests
+
+    // $monitor("Result %d", result);
+    #10;
+    a = 4'b0110; // 6;
+    b = 4'b0101; // 5;
+
+    #10;
+    alucontrol = ADD;
+    #1;
+    $display("result %d, op %b a=%d b=%d\n", result, alucontrol, a, b);
+
+    #10;
+    alucontrol = SUB;
+    #1;
+    $display("result %d, op %b a=%d b=%d\n", result, alucontrol, a, b);
+
+    #10;
+    alucontrol = AND;
+    #1;
+    $display("result %b, op %b a=%b b=%b\n", result, alucontrol, a, b);
+
+    #10;
+    alucontrol = OR;
+    #1;
+    $display("result %b, op %b a=%b b=%b\n", result, alucontrol, a, b);
+
+  end: drive_tests
+
   // TODO:  Add your testbench code here
-  
-endmodule: alu_tb 
- 
+
+endmodule: alu_tb
+
