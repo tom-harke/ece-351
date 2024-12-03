9eef1b25e551c9d89369ac1daa3a0f200d260b90
Tom Harke <tom.harke@gmail.com>
2024-12-03 13:48:39 -0800

    snapshot @ local machine

--- hw4/stackCPU.sv
+++ hw4/stackCPU.sv
@@ -12,7 +12,7 @@
  *
  * Revision History
  * ----------------
- * 1.1.0    (21-Nov-2024 by RK) Added configurable reset polarity.  Support single-stepping
+ * 1.1.0    (21-Nov-2024 by RK)    Added configurable reset polarity.  Support single-stepping
  * 1.0.0    (16-Nov-2024 by RK)    Initial version
  *
  */
@@ -21,48 +21,61 @@ import stackCPU_DEFS::*;
 
 module stackCPU
 #(
-  parameter int DATA_WIDTH  = 32,
-  parameter int STACK_DEPTH = 16,
-  parameter int INSTR_WIDTH = 10,
-  parameter int PC_WIDTH    = 10
-)
-(
-  input  logic clk, reset,
-  input  logic [INSTR_WIDTH-1:0] instruction,
-  output logic [PC_WIDTH-1:0] pc,
-  output logic signed [DATA_WIDTH-1:0] result,
-  output logic error, halt
+  parameter int DATA_WIDTH  = DATA_WIDTH_DEF,     // Width of each stack element
+  parameter int STACK_DEPTH = STACK_DEPTH_DEF,    // Depth of the stack
+  parameter int INSTR_WIDTH = INSTR_WIDTH_DEF,    // Width of an instruction
+  parameter int PC_WIDTH    = PC_WIDTH_DEF,       // Width of the program counter
+
+  parameter int RESET_POLARITY_LOW = 0,           // reset polarity. default is asserted high
+  parameter int SSTEP_ENABLE_DEF   = 0            // enable single step functionality. default is disabled
+)(
+  // global clock and reset
+  input logic                             clk,
+  input logic                             reset,          // Active-high reset
+
+  // instruction from program memory
+  input logic [INSTR_WIDTH-1:0]           instruction,    // 16-bit instruction:
+                                                          // {opcode[4:0], 1'b0, imm[9:0]}
+  output logic [PC_WIDTH-1:0]             pc,             // program counter
+
+  // interface to nexysA7
+  //input logic                             single_step,    // step on instruction on rising edge         // TODO
+  //output logic signed [DATA_WIDTH-1:0]    result,         // ALU result                                 // TODO
+  //output logic                            valid_result,   // asserted high when ALU result is valid     // TODO
+  //output logic                            error,          // error if asserted high                     // TODO
+  //output logic                            halt            // CPU is halted                              // TODO
 );
 
-  opcode_t opcode;
-  state_t current, next;
-  logic unused;
-  logic pop, push;
-  logic signed [9:0] immediate;
-  logic signed [DATA_WIDTH-1:0] top, op1, op2;
-  logic full, empty;
+// internal signals
+logic push, pop;            // stack control
+opcode_t opcode;
+state_t current, next;
+logic unused;
+logic signed [9:0] immediate;
+logic signed [DATA_WIDTH-1:0] top, op1, op2;
+logic full, empty;
 
-  logic HI, LO; // TODO: these could make the code more readable
-  assign HI = 1;
-  assign LO = 1;
+logic HI, LO; // TODO: these could make the code more readable
+assign HI = 1;
+assign LO = 1;
 
-  Stack #(
-    .DATA_WIDTH(DATA_WIDTH),
-    .DEPTH(STACK_DEPTH)
-  ) STACK
-  (
-    .clk(clk),
-    .reset(reset),
-    .push(push),
-    .pop(pop),
-    .data_in(result),
-    .data_out(top),
+Stack #(
+  .DATA_WIDTH(DATA_WIDTH),
+  .DEPTH(STACK_DEPTH)
+) STACK
+(
+  .clk(clk),
+  .reset(reset),
+  .push(push),
+  .pop(pop),
+  .data_in(result),
+  .data_out(top),
 
-    .full(full),
-    .empty(empty),
-    .one_or_more(),  // UNUSED
-    .two_or_more()   // UNUSED
-  );
+  .full(full),
+  .empty(empty),
+  .one_or_more(),  // UNUSED
+  .two_or_more()   // UNUSED
+);
 
 
   always_ff @(posedge clk or posedge reset)
