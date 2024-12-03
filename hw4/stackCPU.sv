43aa554bbd49a8561a8be7b767e0b29e88adf991
Tom Harke <tom.harke@gmail.com>
2024-12-02 16:22:15 -0800

    hw4 snapshot of code from hw3
    Given that I need to refactor my hw3 code to shoehorn it into the hw4
    stubs, I've made an initial copy.
    
    Note that:
      stackCPU.sv
       - this is verbatim from hw3
      stackCPU.sv.target
       - this the sample code from Roy for hw4 (previously called stackCPU.sv)

--- hw4/stackCPU.sv
+++ hw4/stackCPU.sv
@@ -1,105 +1,138 @@
 /**
- * @file stackCPU.sv
- * @brief  Top level for the stack CPU
+ * stackCPU.sv - stack-based CPU
  *
- * @version 1.1.0
- * @author	Roy Kravitz (roy.kravitz@pdx.edu)
- * @date	16-Nov-2024
+ * @author Tom Harke (harke@pdx.edu)
+ * @date   2024 Nov 27
  *
- * @detail
- * Implements the top level module for the stack CPU.  The stack CPU is
- * implemented as an FSM-D with stackCPU_CTL doing the control for the
- * stackCPU_DP datapath
+ * @brief
+ * TODO
+ * 
  *
- * Revision History
- * ----------------
- * 1.1.0	(21-Nov-2024 by RK) Added configurable reset polarity.  Support single-stepping
- * 1.0.0	(16-Nov-2024 by RK)	Initial version
- *
+ * @note:  TODO
  */
- // global definitions, parameters, etc.
+
 import stackCPU_DEFS::*;
 
 module stackCPU
 #(
-	parameter int DATA_WIDTH = DATA_WIDTH_DEF,		// Width of each stack element
-    parameter int STACK_DEPTH = STACK_DEPTH_DEF,	// Depth of the stack
-	parameter int INSTR_WIDTH = INSTR_WIDTH_DEF,	// Width of an instruction
-	parameter int PC_WIDTH = PC_WIDTH_DEF,			// Width of the program counter
-	
-	parameter int RESET_POLARITY_LOW = 0,			// reset polarity. default is asserted high
-	parameter int SSTEP_ENABLE_DEF = 0				// enable single step functionality. default is disabled 
-)(  
-
-	// global clock and reset
-    input logic								clk,
-    input logic								reset,			// Active-high reset
-	
-	// instruction from program memory
-    input logic [INSTR_WIDTH-1:0] 			instruction,	// 16-bit instruction:
-															//{opcode[4:0], 1'b0, imm[9:0]}
-	output logic [PC_WIDTH_DEF-1:0] 		pc,				// program counter
-	
-	// interface to nexysA7
-	input logic								single_step,	// step on instruction on rising edge	
-	output logic signed [DATA_WIDTH-1:0]	result,			// ALU result
-	output logic							valid_result,	// asserted high when ALU result is valid
-	output logic							error,			// error if asserted high
-	output logic							halt			// CPU is halted
+  parameter DATA_WIDTH  = 32,
+  parameter STACK_DEPTH = 16,
+  parameter INSTR_WIDTH = 10,
+  parameter PC_WIDTH    = 10
+)
+(
+  input  logic clk, reset,
+  input  logic [INSTR_WIDTH-1:0] instruction,
+  output logic [PC_WIDTH-1:0] pc,
+  output logic signed [DATA_WIDTH-1:0] result,
+  output logic error, halt
 );
 
-// internal signals
-logic push, pop;			// stack control
-logic alu_to_stack;			// stack input mux
-logic div_by_zero;			// divide-by-zero error condition
-logic [4:0] stack_flags;	//{full, empty, one_or_more, two_or_more, div_by_zero}
-logic reset_in; 			// system reset based on expected polarity of reset
+  opcode_t opcode;
+  state_t current, next;
+  logic unused;
+  logic pop, push;
+  logic signed [9:0] immediate;
+  logic signed [DATA_WIDTH-1:0] top, op1, op2;
+  logic full, empty;
 
-assign reset_in = RESET_POLARITY_LOW ? ~reset : reset; 
+  logic HI, LO; // TODO: these could make the code more readable
+  assign HI = 1;
+  assign LO = 1;
 
-
-// instantiate the datapath
-stackCPU_DP #(
-    .DATA_WIDTH(DATA_WIDTH_DEF),
-    .STACK_DEPTH(STACK_DEPTH_DEF),
-	.INSTR_WIDTH(INSTR_WIDTH_DEF)
-) DP
-(
+  Stack #(
+    .DATA_WIDTH(DATA_WIDTH),
+    .DEPTH(STACK_DEPTH)
+  ) STACK
+  (
     .clk(clk),
-    .reset(reset_in),
-    .instruction(instruction),
+    .reset(reset),
     .push(push),
     .pop(pop),
-    .alu_to_stack(alu_to_stack),
-	.ld_op1(ld_op1),
-	.ld_op2(ld_op2),
-	
-    .result(result),
-	.stack_flags(stack_flags)
-);
+    .data_in(result),
+    .data_out(top),
 
-// instantiate the control FSM
-stackCPU_CTL #(
-	.INSTR_WIDTH(INSTR_WIDTH_DEF),
-	.PC_WIDTH(PC_WIDTH_DEF),
-	.SSTEP_ENABLE(SSTEP_ENABLE_DEF)
-) CP
-(
-    .clk(clk),
-    .reset(reset_in),
-	.instruction(instruction),
-	.pc(pc),
-	.stack_flags(stack_flags),
-    .push(push),
-    .pop(pop),
-    .alu_to_stack(alu_to_stack),
-	.ld_op1(ld_op1),
-	.ld_op2(ld_op2),
-	.single_step(single_step),
-	.valid_result(valid_result),
-	.error(error),
-	.halt(halt)
-);
+    .full(full),
+    .empty(empty),
+    .one_or_more(),  // UNUSED
+    .two_or_more()   // UNUSED
+  );
 
+
+  always_ff @(posedge clk or posedge reset)
+    if (reset)
+      begin
+        pc <= 0;
+        current <= FETCH;
+      end
+    else
+      begin
+        current <= next;
+        if (next==FETCH)
+          begin
+            pc <= pc+1;
+          end
+        if (next==ERROR)
+          begin
+            pc <= pc+1;
+          end
+      end
+
+  always_ff @(posedge clk)
+    // Copy stack data into operand registers.
+    // For the sake of simple code, copy even if it's not needed for the operation.
+    begin
+      case (current)
+        DECODE: op2 <= top; // only used for binary, copied regardless
+        POP2:   op1 <= top; // not used for immediate, copied regardless
+        PUSH: // This part isn't necessary, but it makes it easier to understand the output
+          begin
+            op1 <= 1'bx;
+            op2 <= 1'bx;
+          end
+      endcase
+    end
+
+
+  always_comb // Next State & push/pop control signals
+    begin
+      case (current)
+        FETCH: {next,pop,push} = {DECODE,1'b0,1'b0};
+        DECODE:
+          begin
+            case (opcode)
+              PUSH_IMMEDIATE: {next,pop,push} = {PUSH,1'b0,1'b0}; // TODO want HI/LO instead of 1/0
+              INVERT:         {next,pop,push} = {POP1,1'b1,1'b0};
+              default:        {next,pop,push} = {POP2,1'b1,1'b0};
+            endcase
+            if (opcode==DIV && top==0)
+              next = ERROR;
+          end
+        POP2: {next,pop,push} = {POP1, 1'b1,1'b0};
+        POP1: {next,pop,push} = {PUSH, 1'b0,1'b0};
+        PUSH: {next,pop,push} = {FETCH,1'b0,1'b1};
+      endcase
+      if (push && full) next = ERROR;
+      if (pop && empty) next = ERROR;
+    end
+
+
+  always_comb
+    begin
+      unique case (opcode)
+        PUSH_IMMEDIATE: result = immediate;
+        ADD:            result = op1 + op2;
+        SUB:            result = op1 - op2;
+        MUL:            result = op1 * op2;
+        DIV:            result = op1 / op2;
+        MOD:            result = op1 % op2;
+        AND:            result = op1 & op2;
+        OR:             result = op1 | op2;
+        INVERT:         result =     ~ op2;
+        default:        result = 1'bx; // needed to silence warning
+      endcase
+    end
+
+assign {opcode,unused,immediate} = instruction;
 
 endmodule: stackCPU
---
+++ hw4/tb_stackCPU.sv
@@ -0,0 +1,132 @@
+/**
+ * @file tb_stackCPU.sv
+ * @brief Testbench for stackCPU
+ * 
+ * @submitter   Tom Harke (harke@pdx.edu)
+ * @date        27-Nov-2024
+
+ * @history
+ *    original author: Roy Kravitz (roy.kravitz@pdx.edu)
+ *    date:   16-Nov-2024
+ *
+ * @detail
+ * This is the source code for a testbench that instantiates a stack-based CPU that supports
+ * the following instructions:
+ * 
+ * Opcode   Mnemonic         Operation
+ * 00000    PUSH_IMMEDIATE   Push immediate value onto stack
+ * 00001    ADD              Add top two values on stack
+ * 00010    SUB              Subtract top two values on stack
+ * 00011    MUL              Multiply top two values on stack
+ * 00100    DIV              Divide top two values on stack
+ * 00101    MOD              Modulus (remainder) of top two values on stack
+ * 00110    AND              bitwise AND of top two values on stack
+ * 00111    OR               bitwise OR of top two values on stack
+ * 01000    INVERT           bitwise inversion of the top value on stack
+ *
+ * The instruction format is instruction[15:0] = {opcode[4:0], 1'b0, immediate[10:0]}. 
+ * Arithmetic is 2's complement.
+ *
+ * The testbench reads an output file from the assembler that contains the instructions and
+ * continues to run until the instruction is 'X (meaning we ran out of instructions) or we've run
+ * out of instructions or if the CPU executed an ERROR condition
+ *
+ */
+ 
+  // global definitions, parameters, etc.
+import stackCPU_DEFS::*;
+
+module tb_stackCPU;
+	logic clk;
+	logic reset;
+	logic [DATA_WIDTH_DEF-1:0] program_memory [0:PGRM_MEM_DEPTH_DEF-1]; 
+	logic [INSTR_WIDTH_DEF-1:0] instruction;
+	logic [PC_WIDTH_DEF-1:0] pc;
+	logic signed [DATA_WIDTH_DEF-1:0] result;
+	logic error, halt;
+
+	// Instantiate the stack-based CPU
+	stackCPU #(
+		.DATA_WIDTH(DATA_WIDTH_DEF),
+		.STACK_DEPTH(STACK_DEPTH_DEF),
+		.INSTR_WIDTH(INSTR_WIDTH_DEF),
+		.PC_WIDTH(PC_WIDTH_DEF)
+	) DUT
+	(
+		.clk(clk),
+		.reset(reset),	
+		.instruction(instruction),
+		.pc(pc),
+		.result(result),
+		.error(error),
+		.halt(halt)
+	);   
+
+  import "DPI-C" function string getenv(input string env_name);
+
+	initial begin: display_working_dir
+		$display("ECE 351 Fall 2024: Stack CPU testbench - Tom Harke (harke@pdx.edu)");
+		$display("Sources: %s\n", getenv("PWD"));
+	end: display_working_dir
+
+	// clock generator
+	initial begin: CLK_GEN
+		clk = 0;
+		forever #5 clk = ~clk; // 10ns clock period
+	end: CLK_GEN
+	
+	assign instruction = program_memory[pc];
+
+	// Testbench stimulus
+	initial begin: STIMULUS
+        reset = 1;
+
+        // Load program memory from file while in reset
+        $readmemb("program.mem", program_memory);
+
+        repeat(2) @(posedge clk);	// wait 2 clock periods before starting CPU
+        reset = 0;		
+		$display("StackCPU is alive and executing instructions!!");		
+	end: STIMULUS
+
+	// Monitor results
+	always @(posedge clk) begin: MONITOR
+		if (error && !reset) begin
+			$display("Error occurred during execution.");
+			$stop;
+		end else if (halt && !reset) begin
+			$display("CPU was halted.");
+			$stop;
+		end else if (program_memory[DUT.pc] === 'X) begin
+			$display("Reached end of program.");
+			$stop;
+		end else if (pc > PGRM_MEM_DEPTH_DEF) begin
+			$display("Reached end of memory.");
+			$stop;
+		end else if (~reset) begin
+			//$display("Operation: %15s, (%d)\tState: %4s  (%4s)\t pop=%b push=%b\tPC: +%-10d\ttop: %d"
+			if (DUT.current == FETCH || DUT.current == ERROR)
+				begin
+					$display("----+-----------------------+-----------------+-----------+------+-----------+-------");
+					$display("PC  | Operation             |  State  (next)  | pop  push | top  | op1   op2 | result");
+				end
+			$display("%3d | %14s, (%3d) | %6s (%6s) | %b    %b    | %4d | %4d,%4d | %4d %10b"
+				,DUT.pc
+				,DUT.opcode.name
+				,DUT.immediate
+				,DUT.current.name
+				,DUT.next.name
+				,DUT.pop
+				,DUT.push
+				,DUT.top
+				,DUT.op1
+				,DUT.op2
+				,DUT.result
+				,DUT.result
+			);
+		end else begin
+			$display("waiting to come out of reset");
+		end
+	end: MONITOR
+
+endmodule: tb_stackCPU
