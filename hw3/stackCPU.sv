973f3308d1ba956287e731f9dc50bb117daa38bc
Tom Harke <tom.harke@gmail.com>
2024-12-05 13:09:12 -0800

    snapshot from local
    attempt to resolve differences between hw4 done on remote vivado
    machine with hw3 done on different remote with lightweight simulator

--- hw3/stackCPU.sv
+++ hw3/stackCPU.sv
@@ -15,16 +15,23 @@ import stackCPU_DEFS::*;
 
 module stackCPU
 #(
-  parameter logic SSTEP_ENABLE   = 0,
-  parameter DATA_WIDTH  = 32,
-  parameter STACK_DEPTH = 16,
-  parameter INSTR_WIDTH = 10,
-  parameter PC_WIDTH    = 10
+  parameter int DATA_WIDTH  = DATA_WIDTH_DEF,     // Width of each stack element
+  parameter int STACK_DEPTH = STACK_DEPTH_DEF,    // Depth of the stack
+  parameter int INSTR_WIDTH = INSTR_WIDTH_DEF,    // Width of an instruction
+  parameter int PC_WIDTH    = PC_WIDTH_DEF,       // Width of the program counter
+
+  parameter int SSTEP_ENABLE_DEF   = 0            // enable single step functionality. default is disabled
+
 )
 (
-  input  logic clk, reset,
-  input  logic [INSTR_WIDTH-1:0] instruction,
-  output logic [PC_WIDTH-1:0] pc,
+  // global clock and reset
+  input logic                             clk,
+  input logic                             reset,          // Active-high reset
+
+  // instruction from program memory
+  input logic [INSTR_WIDTH-1:0]           instruction,    // 16-bit instruction:
+                                                          // {opcode[4:0], 1'b0, imm[9:0]}
+  output logic [PC_WIDTH-1:0]             pc,             // program counter
 
   // interface to nexysA7
   input logic                             single_step,    // step on instruction on rising edge
@@ -34,36 +41,36 @@ module stackCPU
   output logic                            halt            // CPU is halted
 );
 
-  opcode_t opcode;
-  state_t current, next;
-  logic unused;
-  logic pop, push;
-  logic signed [9:0] immediate;
-  logic signed [DATA_WIDTH-1:0] top, op1, op2;
-  logic full, empty;
+// internal signals
+logic push, pop;            // stack control
+opcode_t opcode;
+state_t current, next;
+logic unused;
+logic signed [9:0] immediate;
+logic signed [DATA_WIDTH-1:0] top, op1, op2;
+logic full, empty;
 
-  logic HI, LO; // TODO: these could make the code more readable
-  assign HI = 1;
-  assign LO = 1;
+logic HI, LO; // TODO: these could make the code more readable
+assign HI = 1;
+assign LO = 1;
 
-  Stack #(
-    .DATA_WIDTH(DATA_WIDTH),
-    .DEPTH(STACK_DEPTH)
-  ) STACK
-  (
-    .clk(clk),
-    .reset(reset),
-    .push(push),
-    .pop(pop),
-    .data_in(result),
-    .data_out(top),
+Stack #(
+  .DATA_WIDTH(DATA_WIDTH),
+  .DEPTH(STACK_DEPTH)
+) STACK
+(
+  .clk(clk),
+  .reset(reset),
+  .push(push),
+  .pop(pop),
+  .data_in(result),
+  .data_out(top),
 
-    .full(full),
-    .empty(empty),
-    .one_or_more(),  // UNUSED
-    .two_or_more()   // UNUSED
-  );
-
+  .full(full),
+  .empty(empty),
+  .one_or_more(),  // UNUSED
+  .two_or_more()   // UNUSED
+);
 
   always_ff @(posedge clk or posedge reset)
     if (reset)
@@ -75,13 +82,9 @@ module stackCPU
       begin
         current <= next;
         if (next==FETCH)
-          begin
-            pc <= pc+1;
-          end
+          pc <= pc+1;
         if (next==ERROR)
-          begin
-            pc <= pc+1;
-          end
+          pc <= pc+1;
       end
 
   always_ff @(posedge clk)
